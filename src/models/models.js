954e90e75b3ae9813d514fefb498de1451baa9e5
Linus Svensson <linus.svensson@sogeti.se>
2022-08-01 08:47:29 +0200

    initial

---
+++ src/models/models.js
@@ -0,0 +1,18 @@
+import mongoose, { mongo } from 'mongoose';
+const { Schema } = mongoose;
+
+const mySchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    descriptions: {
+        type: String,
+        required: true,
+    },
+    category: { type: String },
+    price: { type: Number },
+    created_date: { type: Date, default: Date.now() }
+});
+
+const myModel = mongoose.model('Model', mySchema);
