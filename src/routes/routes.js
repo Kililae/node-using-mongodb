a61b8adb78a7e17605ed60ddbed821f70113c05b
Linus Svensson <linus.svensson@sogeti.se>
2022-08-01 11:51:26 +0200

    fixed routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,5 +3,5 @@ import { addnewProduct } from '../controllers/controllers';
 export const routes = (app) => {
     // Post endpoint
     app.route('/products')
-    .post(addnewProduct)
+        .post(addnewProduct)
 };
