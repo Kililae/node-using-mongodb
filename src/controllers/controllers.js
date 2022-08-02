7b0438aaf155037b9effb25ee385937b3e46be94
Linus Svensson <linus.svensson@sogeti.se>
2022-08-02 08:26:41 +0200

    module 4

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -10,7 +10,24 @@ export const addnewProduct = (req, res) => {
         if (err) {
             return res.send(err);
         }
-
         return res.json(Product);
     });
+};
+
+export const getProducts = (req, res) => {
+     Product.find({}, (err, Product) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json(Product);
+    });
+};
+
+export const getProductWithID = (req, res) => {
+    Product.findById(req.params.ProductID, (err, Product) => {
+       if (err) {
+           res.send(err);
+       }
+       res.json(Product);
+   });
 };
